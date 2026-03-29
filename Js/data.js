517ca8c59bf53d4541380a63b368d1485236b434
Arbuz-ignor <maksimplov@gmail.com>
2026-03-29 12:32:47 +0300

    Rebalance

---
+++ Js/data.js
@@ -0,0 +1,194 @@
+const cities = [
+  {
+    id: 'rome',
+    country: 'Italy',
+    city: 'Rome',
+    address: 'Via di S. Eufemia, 19, 00187 Roma RM, Italy',
+    hotels: [
+      {
+        id: 'rome-hotel-1',
+        name: 'Hotel 77 Seventy-Seven',
+        price: 65,
+        stars: 4,
+        rooms: 1,
+        image: './img/1rome.png',
+        description: 'Уютный отель недалеко от центра Рима.'
+      },
+      {
+        id: 'rome-hotel-2',
+        name: 'Trevi Palace Luxury Inn',
+        price: 95,
+        stars: 5,
+        rooms: 2,
+        image: './img/1rome.png',
+        description: 'Комфортный вариант рядом с популярными достопримечательностями.'
+      }
+    ]
+  },
+  {
+    id: 'cair',
+    country: 'Egypt',
+    city: 'Cairo',
+    address: 'Downtown Cairo, Cairo Governorate, Egypt',
+    hotels: [
+      {
+        id: 'cair-hotel-1',
+        name: 'Cairo Paradise Hotel',
+        price: 40,
+        stars: 3,
+        rooms: 1,
+        image: './img/cair1.png',
+        description: 'Доступный вариант в центре города.'
+      },
+      {
+        id: 'cair-hotel-2',
+        name: 'Golden Pyramids Inn',
+        price: 58,
+        stars: 4,
+        rooms: 2,
+        image: './img/cair2.png',
+        description: 'Удобный отель с просторными номерами.'
+      }
+    ]
+  },
+  {
+    id: 'paris',
+    country: 'France',
+    city: 'Paris',
+    address: 'Rue Saint-Honoré, Paris, France',
+    hotels: [
+      {
+        id: 'paris-hotel-1',
+        name: 'Paris Opera Residence',
+        price: 110,
+        stars: 4,
+        rooms: 1,
+        image: './img/paris1.png',
+        description: 'Стильный отель рядом с центром Парижа.'
+      },
+      {
+        id: 'paris-hotel-2',
+        name: 'Eiffel View Hotel',
+        price: 145,
+        stars: 5,
+        rooms: 2,
+        image: './img/paris1.png',
+        description: 'Элегантный вариант для комфортного отдыха.'
+      },
+      {
+        id: 'paris-hotel-3',
+        name: 'Louvre City Rooms',
+        price: 98,
+        stars: 4,
+        rooms: 1,
+        image: './img/paris1.png',
+        description: 'Уютные номера в туристическом районе.'
+      },
+    {
+        id: 'rome-hotel-3',
+        name: 'Roma Paradise Hotel',
+        price: 72,
+        stars: 4,
+        rooms: 1,
+        image: './img/1rome.png',
+        description: 'Спокойный отель для туристической поездки.'
+      }
+    ]
+  },
+  {
+    id: 'madrid',
+    country: 'Spain',
+    city: 'Madrid',
+    address: 'Gran Vía, Madrid, Spain',
+    hotels: [
+      {
+        id: 'madrid-hotel-1',
+        name: 'Gran Via Suites',
+        price: 76,
+        stars: 4,
+        rooms: 1,
+        image: './img/madrid1.png',
+        description: 'Современный отель в оживлённой части города.'
+      },
+      {
+        id: 'madrid-hotel-2',
+        name: 'Royal Madrid Inn',
+        price: 102,
+        stars: 5,
+        rooms: 2,
+        image: './img/madrid1.png',
+        description: 'Комфортный вариант для отдыха и прогулок.'
+      },
+      {
+        id: 'madrid-hotel-3',
+        name: 'Sol Central Hotel',
+        price: 69,
+        stars: 3,
+        rooms: 1,
+        image: './img/madrid1.png',
+        description: 'Практичный вариант рядом с транспортом.'
+      }
+    ]
+  },
+  {
+    id: 'ankara',
+    country: 'Turkey',
+    city: 'Ankara',
+    address: 'Kızılay, Ankara, Turkey',
+    hotels: [
+      {
+        id: 'ankara-hotel-1',
+        name: 'Ankara Central Hotel',
+        price: 55,
+        stars: 3,
+        rooms: 1,
+        image: './img/ankara1.png',
+        description: 'Удобный отель в центральной части города.'
+      },
+      {
+        id: 'ankara-hotel-2',
+        name: 'Royal Ankara Suites',
+        price: 78,
+        stars: 4,
+        rooms: 2,
+        image: './img/ankara2.png',
+        description: 'Хороший вариант для спокойного и комфортного отдыха.'
+      },
+      {
+        id: 'ankara-hotel-3',
+        name: 'Capital View Inn',
+        price: 92,
+        stars: 5,
+        rooms: 2,
+        image: './img/ankara3.png',
+        description: 'Более премиальный отель с просторными номерами.'
+      }
+    ]
+  },
+  {
+    id: 'aphine',
+    country: 'Greece',
+    city: 'Athens',
+    address: 'Syntagma Square, Athens, Greece',
+    hotels: [
+      {
+        id: 'aphine-hotel-1',
+        name: 'Athens City Hotel',
+        price: 67,
+        stars: 4,
+        rooms: 1,
+        image: './img/aphine1.png',
+        description: 'Уютный вариант для поездки по историческому центру.'
+      },
+      {
+        id: 'aphine-hotel-2',
+        name: 'Acropolis Comfort Rooms',
+        price: 89,
+        stars: 4,
+        rooms: 2,
+        image: './img/aphine1.png',
+        description: 'Комфортный отель недалеко от популярных мест.'
+      }
+    ]
+  }
+];
--- Js/main.js
+++ Js/main.js
@@ -42,29 +42,62 @@ buttonChoice.forEach((button) => {
 //Комнат: 3
 //Цена.сут: 4000р
 
-const cityInfo = {
-    cair: [
-        {
-            number: 323,
-            stars: '✮✮✮✮',
-            rooms: 3,
-            price: '4000р',
-            img: './img/cair1.png'
-        },
-        {
-            number: 201,
-            stars: '✮✮✮',
-            rooms: 2,
-            price: '3200р',
-            img: './img/cair2.png'
-        }
-    ],
-    rome: [
-        {
-            number: 145,
-            stars: '✮✮✮✮✮',
-            rooms: 4,
-            price: '6500р'
-        }
+const cities = [
+  {
+    id: 'rome',
+    country: 'Italy',
+    city: 'Rome',
+    address: 'Via di S. Eufemia, 19, 00187 Roma RM, Italy',
+    hotels: [
+      {
+        id: 'rome-hotel-1',
+        name: 'Hotel 77 Seventy-Seven - Maison D’Art Collection',
+        price: 65,
+        stars: 4,
+        rooms: 1,
+        image: './images/rome/1.jpg',
+        description: 'Уютный отель в центре Рима.'
+      },
+      {
+        id: 'rome-hotel-2',
+        name: 'Trevi Palace Luxury Inn',
+        price: 95,
+        stars: 5,
+        rooms: 2,
+        image: './images/rome/2.jpg',
+        description: 'Отель рядом с основными достопримечательностями.'
+      }
     ]
-};
+  },
+  {
+    id: 'cair',
+    country: 'Egypt',
+    city: 'Cairo',
+    address: 'Cairo, Egypt',
+    hotels: [
+      {
+        id: 'cair-hotel-1',
+        name: 'Cairo Paradise Hotel',
+        price: 40,
+        stars: 3,
+        rooms: 1,
+        image: './images/cairo/1.jpg',
+        description: 'Простой и удобный вариант для поездки.'
+      }
+    ]
+  },
+  {
+    id: 'paris',
+    country: 'France',
+    city: 'Paris',
+    address: 'Paris, France',
+    hotels: []
+  },
+  {
+    id: 'madrid',
+    country: 'Spain',
+    city: 'Madrid',
+    address: 'Madrid, Spain',
+    hotels: []
+  }
+];
